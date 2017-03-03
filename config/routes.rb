76c30de800b1b2ccb1feed85ddf9d92f8fb1d361
Cody Brubaker <cody.brubaker@gmail.com>
2017-03-03 12:30:46 -0800

    fix rails namespace bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  mount RailsAdmin::Engine => '/administrator', as: 'rails_admin'
   devise_for :admins
   root to: "landing#index"
   resources :about, :only => ['index']
