6568232a1e593f67efc34cf9fdfe7bc6d6e2c8a1
Cody Brubaker <cody.brubaker@gmail.com>
2017-03-03 12:19:53 -0800

    make projects links

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -4,19 +4,6 @@ class AboutController < ApplicationController
     @abouts = About.all
   end
 
-  def edit
-    @about = About.find(params[:id])
-  end
-
-  def update
-    @about = About.find(params[:id])
-    if @about.update(about_params)
-      redirect_to about_index_path
-    else
-      render :edit
-    end
-  end
-
 
   private
   def about_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :admins
   root to: "landing#index"
-  resources :about
+  resources :about, :only => ['index']
   resources :repos
 
 end
